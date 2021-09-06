cce858f6a1c89c5a8f5951005ffdd44e323458c1
James Cabeso <jamescabeso@jamessponalmbp2.home>
2021-09-06 22:06:22 +0100

    creating our first user data type

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,34 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+// logging in keystone backend
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long should they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  // @ts-ignore
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    //  TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // schema items go in here
+  }),
+  ui: {
+    // TODO change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO add session values here
+});
