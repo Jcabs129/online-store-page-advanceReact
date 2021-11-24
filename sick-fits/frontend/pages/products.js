2964b769d5727c00d893da9a5d4026eaadaf62bf
James Cabeso <jamescabeso@jamessponalmbp2.home>
2021-11-24 16:36:14 +0000

    Mod-7 pagination: 32 pagination links

--- sick-fits/frontend/pages/products.js
+++ sick-fits/frontend/pages/products.js
@@ -1,9 +1,12 @@
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 export default function OrderPage() {
   return (
     <div>
+      <Pagination page={11} />
       <Products />
+      <Pagination page={1} />
     </div>
   );
 }
