a2a0b011ae258e7668dcc745860180b1b35d31fd
James Cabeso <jamescabeso@jamessponalmbp2.home>
2021-11-25 16:30:49 +0000

    Mod-7 pagination: fixing styling /componenets/page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -14,13 +14,13 @@ const GlobalStyles = createGlobalStyle`
   html {
     --red: #ff0000;
     --black: #393939;
-    --grey: #3a3a3a;
-    --gray: var(--grey);
-    --lightgray: #e1e1e1;
-    --lightgray: var(--lightGrey);
+    /* --grey: #3a3a3a; */
+    --grey: var(--grey);
+    /* --lightgrey: #e1e1e1; */
+    --lightGrey: var(--lightGray);
     --offWhite: #ededed;
     --maxWidth: 1000px;
-    --bs: '0 12px 24px 0 rgba(0,0,0,0.09)'
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
     box-sizing: border-box;
     font-size: 10px;
   }
