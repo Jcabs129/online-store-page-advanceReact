b844a182ffb7a46368681a7090556cec99f9966e
James Cabeso <jamescabeso@jamessponalmbp2.home>
2021-12-22 11:15:51 +0000

    mod-9_vid-46-adding-items-to-cart

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -11,6 +11,7 @@ import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -66,6 +67,7 @@ export default withAuth(
       CartItem,
     }),
 
+    extendGraphqlSchema,
     ui: {
       // show the UI only for people who pass this test
       isAccessAllowed: ({ session }) =>
