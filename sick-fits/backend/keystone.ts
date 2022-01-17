b02acdb47aae0c48c524a033ba309c43586e3081
James Cabeso <jamescabeso@jamessponalmbp2.home>
2022-01-17 22:33:20 +0000

    mod-11 roles, permissions and restricting access: vid-62 creatig the roles and permissions schema + UI

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -15,6 +15,7 @@ import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 import { extendGraphqlSchema } from './mutations';
+import { permissionsList } from './schemas/fields';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -82,7 +83,7 @@ export default withAuth(
     },
     session: withItemData(statelessSessions(sessionConfig), {
       // GraphQL Query
-      User: 'id name email',
+      User: `id name email role { ${permissionsList.join(' ')} }`,
     }),
   })
 );
