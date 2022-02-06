bdd782b6a45bf7ff96f065eae8c0b5b8313654a7
James Cabeso <jamescabeso@jamessponalmbp2.home>
2022-02-06 21:15:16 +0000

    mod-12 testing : vid-79 Testing Signed in and signed out nav states TBC

---
+++ sick-fits/frontend/__tests__/Nav.test.js
@@ -0,0 +1,96 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import Nav from '../components/Nav';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { fakeUser, fakeCartItem } from '../lib/testUtils';
+import { CartStateProvider } from '../lib/cartState';
+
+// Make some mocks for being logged out, logged in, and logged in with cart items
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { authenticatedItem: null } },
+  },
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { authenticatedItem: fakeUser() } },
+  },
+];
+
+const signedInMocksWithCartItems = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        authenticatedItem: fakeUser({
+          cart: [fakeCartItem()],
+        }),
+      },
+    },
+  },
+];
+
+console.log(signedInMocksWithCartItems);
+
+describe('<Nav/>', () => {
+  it('Renders out minimal nav when signed out', () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider mocks={notSignedInMocks}>
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>
+    );
+    // Practise 👇
+    /*
+    const signInText = screen.getByText('Sign In');
+    expect(signInText).toBeInTheDocument();
+
+    const link = container.querySelector('a');
+    expect(link).toHaveAttribute('href', '/signin');
+    */
+    debug();
+
+    expect(container).toHaveTextContent('Sign In');
+    expect(container).toMatchSnapshot();
+
+    const signInLink = screen.getByText('Sign In');
+    expect(signInLink).toHaveAttribute('href', '/signin');
+
+    const productsLink = screen.getByText('Products');
+    expect(productsLink).toHaveAttribute('href', '/products');
+  });
+
+  it('renders a full nav when signed in', async () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider mocks={signedInMocks}>
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>
+    );
+    await screen.findByText('Account');
+    // debug();
+
+    expect(container).toMatchSnapshot();
+    expect(container).toHaveTextContent('Sign Out');
+    expect(container).toHaveTextContent('My Cart');
+  });
+
+  it('renders the amount of items in the cart', async () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider mocks={signedInMocksWithCartItems}>
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>
+    );
+    await screen.findByText('Account');
+    debug();
+
+    expect(screen.getByText('3')).toBeInTheDocument();
+  });
+});
