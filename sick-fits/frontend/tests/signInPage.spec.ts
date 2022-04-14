64a2b9943762f4389e54746b84ccfacf75bb7303
James Cabeso <jamescabeso@jamessponalmbp2.home>
2022-04-14 13:09:13 +0100

    updating playwright test signIn.spec.ts

--- sick-fits/frontend/tests/signInPage.spec.ts
+++ sick-fits/frontend/tests/signInPage.spec.ts
@@ -1,10 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('Test signin page', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:7777/');
   await page.click('text=Sign in');
   await page.fill('#__next input[type=email]', 'test@example.com');
   await page.fill('#__next input[type=password]', 'testing123');
   await page.click('#__next button');
   await expect(page.locator('text=Sign Out').first()).toBeVisible();
+});
+
+test('', async ({ page }) => {
+  await page.click('#__next button:nth-child(6)');
+  // await page.locator('text=Check Out Now!!').click();
+  const cardNum = await page
+    .frameLocator('iframe[name="__privateStripeFrame5985"]')
+    .locator('[placeholder="Card\\ number"]')
+    .click();
+  await cardNum;
+  // .locator('[placeholder="Card\\ number"]')
+  // .click()
 });
